ad6426ca24474fc29142c1fe811210e346e5551a
Rashedul Raha <rashedulraha.bd@gmail.com>
2026-07-17 12:33:30 +0600

    feat: configure axios API client with auth interceptor

---
+++ src/lib/axios.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  timeout: 10000,
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+    }
+    return Promise.reject(error);
+  },
+);
