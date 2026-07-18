9064e7fa6126e67d4e4e4632df4e64d61a6a60d9
Rashedul Raha <rashedulraha.bd@gmail.com>
2026-07-18 17:55:50 +0600

    feat: add useSession API service and remove get_userdata service

--- src/axios/axison_service/get_userdata.service.ts
+++
@@ -1,4 +0,0 @@
-export const getUserData = async () => {
-  try {
-  } catch (error) {}
-};
---
+++ src/axios/axison_service/useSession.service.ts
@@ -0,0 +1,14 @@
+import { api } from "@/lib/axios";
+
+export const useSession = async () => {
+  try {
+    //   console.log(userData);
+    const { data } = await api.post("/api/me");
+    //   console.log(data);
+    return data;
+  } catch (error: any) {
+    console.log("Status:", error.response?.status);
+    console.log("Data:", error.response?.data);
+    throw error;
+  }
+};
