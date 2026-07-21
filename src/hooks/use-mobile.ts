fb7a8e2bf18e9413c8b247f13353e3f3187d51fe
Rashedul Raha <rashedulraha.bd@gmail.com>
2026-07-21 17:06:05 +0600

    style: update UI components and hooks

--- src/hooks/use-mobile.ts
+++ src/hooks/use-mobile.ts
@@ -11,7 +11,10 @@ export function useIsMobile() {
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
     }
     mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    const initialMatch = mql.matches
+    setTimeout(() => {
+      setIsMobile(initialMatch)
+    }, 0)
     return () => mql.removeEventListener("change", onChange)
   }, [])
 
